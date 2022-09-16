162c23e236a91374b90d3a700175419b28e08675
Peculiar-Michael <chiamakamichael2001@gmail.com>
2022-09-16 14:58:53 -0700

    a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*mul - a function tbat multiplies two integers
+*@a: first integer
+*@b: second integer
+*Return: 0
+*/
+
+int mul(int a, int b)
+{
+	_putchar(a * b);
+	return (0);
+}
