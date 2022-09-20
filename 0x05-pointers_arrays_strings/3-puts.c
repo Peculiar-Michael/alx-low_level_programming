a6c004ce2040a2631fd485816cc23afc36760aed
Peculiar-Michael <chiamakamichael2001@gmail.com>
2022-09-19 20:54:57 -0700

    function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts - a function that prints a string until the first newline character
+*@str: the pointer
+*Return: nothing
+*/
+
+void _puts(char *str)
+{
+	while i = 0;
+	while (*str[i] != '\n' && s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+}
